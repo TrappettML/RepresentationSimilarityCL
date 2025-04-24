6901c427ed4c76de00ba96e32909fc2dee8de908
Matthew Trappett <mtrappet@uoregon.edu>
2025-04-24 16:07:45 -0700

    ''

--- experiments/student_teacher/slurm_sparse_loops.sh
+++ experiments/student_teacher/slurm_sparse_loops.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # slurm_sparse_loops.sh > sparse_params.txt
-human_date=$(date +"%a_%d_%b_%Y_%h")    # Human-friendly: Thu-05-Oct-2023
+human_date=$(date +"%a_%d_%b_%Y")    # Human-friendly: Thu-05-Oct-2023
 TALAPAS_PATH="/home/mtrappet/tau/StudentTeacher/single_layer_sparse/run_date_${human_date}/"
 
 d_hs=200
 sparsities=(0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9) #(0.0 0.5) # 
 
 for sparsity in "${sparsities[@]}"; do
-    echo " $d_hs $sparsity \"determ\" \"$TALAPAS_PATH\""
-    echo " $d_hs $sparsity \"random\" \"$TALAPAS_PATH\""
+    echo " $d_hs $sparsity determ \"$TALAPAS_PATH\""
+    echo " $d_hs $sparsity random \"$TALAPAS_PATH\""
 done
